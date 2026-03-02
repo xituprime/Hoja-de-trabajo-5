2c868c5c7ebd1460bc29bfc3333381f7698176f3
xituprime <xit25783@uvg.edu.gt>
2026-03-02 12:10:48 -0600

    agrego Procesos.py

---
+++ proceso.py
@@ -0,0 +1,43 @@
+import random
+
+class Proceso:
+    def _init_(self, id_proceso, env, simulacion):
+        self.id_proceso = id_proceso
+        self.env = env
+        self.simulacion = simulacion
+
+        self.memoria_requerida = random.radint(1,10)
+        self.instrucciones_totales = random.radint(1,10)
+        self.intrucciones_restantes = self.instrucciones_totales
+
+        self.tiempo_llegada = env.now
+        self.tiempo_salida = None
+
+    def ejecutar(self):
+
+        yield self.sim.ram.get(self.memoria_requerida)
+
+        while self.intrucciones_restantes > 0:
+
+            with self.sim.cpu.request() as req:
+                yield req
+
+                intrucciones = min(
+                    self.sim.velocidad_cpu,
+                    self.intrucciones_restantes
+                )
+
+                yield self.env.timeout(1)
+                self.intrucciones_restantes -= intrucciones
+
+            if self.intrucciones_restantes > 0:
+                decision = random.radint(1, 21)
+                if decision == 1:
+                    yield self.env.timeout(1)
+
+        self.tiempo_salida = self.env.now
+
+        yield self .sim.ram.put(self.memoria_requerida)
+
+        tiempo_total = self.tiempo_salida - self.tiempo_llegada
+        self.sim.tiempos.append(tiempo_total)
